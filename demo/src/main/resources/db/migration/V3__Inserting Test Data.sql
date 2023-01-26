0c940d9c067ef0242dc9fb5ecef887a77f8f8e8f
Rahman <margub.9363@gmail.com>
2023-01-26 17:01:57 +0530

    Data Insertion

---
+++ demo/src/main/resources/db/migration/V3__Inserting Test Data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `flywayschema`.`registration_users` (`id`, `userName`, `firstName`, `lastName`, `email`, `mobile`) VALUES ('1', 'test', 'test', 'test', 'test', 'test');
---
+++ demo/src/main/resources/db/migration/V4__Inserting Test Data.sql
@@ -0,0 +1,1 @@
+INSERT INTO `flywayschema`.`registration_users` (`id`, `userName`, `firstName`, `lastName`, `email`, `mobile`) VALUES ('2', 'testing', 'testing', 'testing', 'testing', 'testing');
