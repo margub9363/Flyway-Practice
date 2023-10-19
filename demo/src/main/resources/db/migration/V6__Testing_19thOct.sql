849497bf4efe1dad072ad3990e19e931053d100a
margub9363 <69838623+margub9363@users.noreply.github.com>
2023-10-19 10:49:43 +0530

    Testing

---
+++ demo/src/main/resources/db/migration/V6__Testing_19thOct.sql
@@ -0,0 +1,1 @@
+INSERT INTO `flywayschema`.`registration_users` (`id`, `userName`, `firstName`, `lastName`, `email`, `mobile`) VALUES ('6', 'testing 19th Ovt', 'testing', 'testing', 'testing', 'testing');
